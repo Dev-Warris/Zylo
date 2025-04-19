b5317c5189c17a4f2090ae0ef4e94a0bb38d6070
Warris <warris.yt.007@gmail.com>
2025-04-19 04:12:41 +0200

    Update Import.bat

--- Import.bat
+++ Import.bat
@@ -1,4 +1,2 @@
-@echo off
-echo Importing BIOS settings...
-SCEWIN_64.exe /i /s "%ProgramData%\warris\BIOSSettings.txt" /q /hb /ni
-echo BIOS settings imported successfully.
+SCEWIN_64.exe /I /S BIOSSettings.txt
+pause
